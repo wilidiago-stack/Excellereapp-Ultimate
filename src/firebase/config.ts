0feb3dc3962c014101dad4895aea0fdf68ff5912
William Diago <parcerosconstructioninfo@gmail.com>
2026-02-27 18:05:19 +0000

    Set up a Firebase backend

---
+++ src/firebase/config.ts
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+  "projectId": "studio-8645052572-21c21",
+  "appId": "1:366012754133:web:553874dc44acad72325a5c",
+  "apiKey": "AIzaSyCPJa9-WhaSGIhwLkIvQFWR4MOA5SZfwds",
+  "authDomain": "studio-8645052572-21c21.firebaseapp.com",
+  "measurementId": "",
+  "messagingSenderId": "366012754133"
+};
